b8a413a9ce8c00c6abd27af3b0538ded116b13cf
HuynhGiaBao1409 <giabao1409203@gmail.com>
2024-01-19 17:52:19 +0700

    test

---
+++ src/Tiil/edu/XinChao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Tiil.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// 
+		System.out.println("Hello World");
+	}
+
+}
