6b90c3f2995f9d0c3afe1b991cfe958523c55e67
HuynhGiaBao1409 <giabao1409203@gmail.com>
2024-01-23 17:00:17 +0700

    Bai2

--- Lab2/src/Bai2.java
+++ Lab2/src/Bai2.java
@@ -5,6 +5,7 @@
 /**
  * 
  */
+import java.util.Scanner;
 public class Bai2 {
 
 	/**
@@ -12,7 +13,26 @@ public class Bai2 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner =new Scanner(System.in);
+		System.out.print("Nhập a: ");
+		double a = scanner.nextDouble();
+		System.out.print("Nhập b: ");
+		double b = scanner.nextDouble();
+		System.out.print("Nhập c: ");
+		double c = scanner.nextDouble();
+		double delta=Math.pow(b,2)-4*a*c;
+		if(a==0) {
+			System.out.printf("PT có nghiêm x = %f",-c/b);
+		}else {
+			if(delta<0)
+				System.out.println("PT vô nghiệm");
+			if(delta==0)
+				System.out.printf("PT có nghiệm kép x = %f",-b/(2*a));
+			else {
+				System.out.printf("PT có 2 nghiệm x1 = %f\n",(-b+Math.sqrt(delta))/(2*a));
+				System.out.printf("PT có 2 nghiệm x2 = %f",(-b-Math.sqrt(delta))/(2*a));
+			}
+		}
 	}
 
 }
