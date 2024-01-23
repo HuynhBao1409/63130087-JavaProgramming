f7d7ab617a00633ba5d519fb8f52ee790f9b6ed3
HuynhGiaBao1409 <giabao1409203@gmail.com>
2024-01-23 17:02:17 +0700

    Bai3

--- Lab2/src/Bai3.java
+++ Lab2/src/Bai3.java
@@ -5,6 +5,7 @@
 /**
  * 
  */
+import java.util.Scanner;
 public class Bai3 {
 
 	/**
@@ -12,7 +13,15 @@ public class Bai3 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner =new Scanner(System.in);
+		System.out.print("Nhập số điện: ");
+		double sodien = scanner.nextDouble();
+		System.out.print("Nhập số tháng: ");
+		double thang =scanner.nextDouble();
+		if(thang<50)
+			System.out.printf("Tiền điện tháng %.1f",sodien*1000);
+		else
+			System.out.printf("Tiền điện tháng %.1f",50*1000+(sodien-50)*1200);
 	}
 
 }
