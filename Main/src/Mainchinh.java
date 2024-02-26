6ffcfbb77ff64105a265908397b704941a5eb6bb
HuynhGiaBao1409 <giabao1409203@gmail.com>
2024-02-26 07:57:00 +0700

    next

--- Main/src/Mainchinh.java
+++ Main/src/Mainchinh.java
@@ -10,15 +10,33 @@ public class Mainchinh {
 		f.setTitle ("Vi du JFrame"); //Tieu đe cua
 		f.setDefaultCloseOperation (WindowConstants. EXIT_ON_CLOSE) ;
 		f.setSize (300,200) ;
-
+		//Qui dinh loai bo cuc (Layout)
+		f.setLayout (new FlowLayout());
+		
 		// Tạo một nút bam
 		JButton btnOK = new JButton ("Nhan toi di");
-		JButton btnkhac = new JButton ("Nhan em di") ;
+		// Tao tai nghe
+		BoLangNgheOK ok = new BoLangNgheOK () ;
+		// gan vao cho nút OK
+		btnOK.addActionListener(ok);
+		JButton btnkhac = new JButton ("Nhan em di");
 		// gắn lên form
 		f.add (btnOK) ;
 		f.add (btnkhac);
 		// Chay thu thì Em đa de lên Toi
 		// hiện form/frame
-		f.show (); // hiện cua
+		f.show (); // hiện cua so
+	}
+	// Tao bộ lắng nghe (hữu danh) va xử lý cho nút OK
+	private static class BoLangNgheOK implements ActionListener
+
+	@override
+	public void actionPerformed (ActionEvent e) {
+	// Mã xử lý khi user click chuột ở đây
+	JDialog dialog = new JDialog ();
+	dialog.setTitle ("Thông bao gì đó");
+	dialog.setSize (220, 120) ;
+	dialog. show () ;
 	}
 }
+
